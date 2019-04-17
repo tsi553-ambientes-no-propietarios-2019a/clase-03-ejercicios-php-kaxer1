68a78fd3366591defb3044f2cec7c7566af4ced3
kaxer1 <kevin.curay@epn.edu.ec>
2019-04-16 19:23:16 -0500

    commit ejercicio2

--- ejercicio2/index.php
+++ ejercicio2/index.php
@@ -4,4 +4,28 @@
  *
  * Implementar una función que muestre un título centrado en pantalla, y la llamaremos posteriormente dos veces.
 */
+
+    function titulo(){
+        echo "<h1> <center>";
+        echo "título centrado" ;
+        echo "</center> </h1>";
+    }
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>EJERCICIO2</title>
+</head>
+<body>
+
+    <br>
+    <?php titulo() ?>
+    <?php titulo() ?>
+
+</body>
+</html>
